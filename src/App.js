82cdb22d3347e97e0d4f830861f14016ace34305
chema12 <hanking132@hotmail.com>
2019-02-06 19:33:21 +0100

    Ejercicio 2 donde modificamos la información que se muestra

--- src/App.js
+++ src/App.js
@@ -7,22 +7,40 @@ import List from './list';
 
 class App extends Component {
   render() {
+    const lista = [];
+    for (let i = 1; i <= 5; i++) {
+      let existUsers = false;
+      const filaUsuario = (
+        <div key={i+'-div'} className="list-layout">
+          <List key={this.props.data.movies[i].name} mainItem etiqueta={`${this.props.data.movies[i].name}`} />
+          <List key={this.props.data.movies[i].name+'intro'} level={1} etiqueta={'Liked By:'} />
+          {            
+            this.props.data.profiles.map((element, index) => {              
+              if(element.favoriteMovieID == i) {
+                existUsers = true;
+                return (
+                  <List key={i+index+'B'} level={2} etiqueta={`- ${this.props.data.users[element.userID].name}`}/>  
+                );
+              }
+            })
+          }
+        </div>
+      );
+      const withoutUsers = (
+        <div key={i+'-div'} className="list-layout">
+          <List key={this.props.data.movies[i].name} mainItem etiqueta={this.props.data.movies[i].name} />
+          <List key={this.props.data.movies[i].name+'outOfUsers'} level={1} etiqueta={'None of the current users liked this movie'} />
+        </div>
+      );          
+      if (existUsers) lista.push(filaUsuario);
+      else lista.push(withoutUsers);
+    }
     return (
       <div className="App">
         <header className="App-header">
           <ListLayout>
-            <h2 className="list-layout">LISTA DE USUARIOS</h2>
-            {
-              this.props.data.profiles.map(element => {
-                return (                  
-                  <div className="list-layout">                    
-                    <List mainItem etiqueta={`${this.props.data.users[element.userID].name}:`} />
-                    <List etiqueta={`Usuario: ${this.props.data.users[element.userID].userName}`}/>                  
-                    <List etiqueta={`Película Favorita: ${this.props.data.movies[element.favoriteMovieID].name}`}/>                  
-                  </div>
-                );
-              })
-            }
+            <h2 className="list-layout">LISTA DE PELÍCULAS</h2>
+            {lista}
           </ListLayout>          
         </header>
       </div>
--- src/list.js
+++ src/list.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
-const List = props => {
+const List = props => {    
+    const pad = props.level * 30;
+
     if (props.mainItem) {
         return <div className="mainItem">{props.etiqueta}</div>;
     } else {
-        return <div className="secondaryItem">{props.etiqueta}</div>;
+        return <div className="secondaryItem" style={{paddingLeft: pad+'px'}}>{props.etiqueta}</div>;
     }
+};
+
+List.propTypes = {
+    level: PropTypes.number
+};
+List.defaultProps  = {
+    level: 1
 };
 
 export default List;
